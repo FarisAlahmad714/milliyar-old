6bb2126746812d09b64e91aa461578bd94a4f56e
Basir <basir.jafarzadeh@gmail.com>
2020-08-09 16:56:45 +0430

    fix data.js

---
+++ backend/data.js
@@ -0,0 +1,46 @@
+export default {
+  products: [
+    {
+      _id: "1",
+      name: "Haj Benjamin Masked",
+      image: "/images/Pal2.png",
+      category: "Benjamins Collection",
+      price: "50",
+    },
+    {
+      _id: "2",
+      name: "Haj Benjamin Bearded",
+      image: "/images/Pal3.png",
+      category: "Benjamins Collection",
+      price: "50",
+    },
+    {
+      _id: "3",
+      name: "Haj Benjamin French Kissed",
+      image: "/images/Pal4.png",
+      category: "Benjamins Collection",
+      price: "50",
+    },
+    {
+      _id: "4",
+      name: "Haj Benjamin Masked",
+      image: "/images/Jor2.jpg",
+      category: "Benjamins Collection",
+      price: "50",
+    },
+    {
+      _id: "5",
+      name: "Haj Benjamin Bearded",
+      image: "/images/Jor3.jpg",
+      category: "Benjamins Collection",
+      price: "50",
+    },
+    {
+      _id: "6",
+      name: "Haj Benjamin French Kissed",
+      image: "/images/Jor4.jpg",
+      category: "Benjamins Collection",
+      price: "50",
+    },
+  ],
+};
